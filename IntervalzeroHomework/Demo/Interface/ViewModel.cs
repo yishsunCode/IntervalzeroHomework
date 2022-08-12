67e31310e8ae6297b2cc7df86a402147d26a8312
LAPTOP-9V17EEST\Mark <yishsunsuncode@gmail.com>
2022-08-12 14:49:15 +0800

    feat: 介面定義

---
+++ IntervalzeroHomework/Demo/Interface/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Demo.Interface
+{
+    public interface IDemoViewModel : INotifyPropertyChanged
+    {
+        ICommand ChooseFolderCommand { get; }
+
+        IList<string> ArticleList { get; }
+        string SelectedArticle { get; set; }
+        ICommand PracticeCommand { get; }
+
+        IList<string> ReplayItemList { get; }
+        string SelectedRelayItem { get; set; }
+        ICommand ReplayCommand { get; }
+
+        object CurrentFrame { get; }
+    }
+
+    public interface IInputFrame : INotifyPropertyChanged
+    {
+        bool IsValid { get; }
+        string UserText { get; set; }
+    }
+    public interface IReplayFrame : INotifyPropertyChanged
+    {
+        string UserText { get; }
+    }
+    public interface IWaitingFrame 
+    {
+        
+    }
+}
