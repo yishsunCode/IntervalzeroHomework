c58fa1c92fe54a22100987af6de09ee69c70d62c
LAPTOP-9V17EEST\Mark <yishsunsuncode@gmail.com>
2022-08-12 16:08:12 +0800

    feat(Utility): 介面轉DataTemplate選擇器

---
+++ IntervalzeroHomework/Utility/Extension/InterfaceDataTemplateSelector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Utility.Extension
+{
+    public class InterfaceDataTemplateSelector : DataTemplateSelector
+    {
+        public static DataTemplateSelector Instance { get; } = new InterfaceDataTemplateSelector();
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            #region Require
+
+            if (item == null) throw new ArgumentNullException();
+            if (container == null) throw new ArgumentNullException();
+
+            #endregion
+
+            // Result
+            DataTemplate itemDataTemplate = null;
+
+            // Base DataTemplate
+            itemDataTemplate = base.SelectTemplate(item, container);
+            if (itemDataTemplate != null) return itemDataTemplate;
+
+            // Interface DataTemplate
+            FrameworkElement itemContainer = container as FrameworkElement;
+            if (itemContainer == null) return null;
+
+            foreach (Type itemInterface in item.GetType().GetInterfaces())
+            {
+                itemDataTemplate = itemContainer.TryFindResource(new DataTemplateKey(itemInterface)) as DataTemplate;
+                if (itemDataTemplate != null) break;
+            }
+
+            // Return
+            return itemDataTemplate;
+        }
+    }
+}
