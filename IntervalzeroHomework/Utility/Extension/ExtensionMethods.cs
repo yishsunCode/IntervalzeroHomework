1fbf2f06560f00382b605ec86f55835a8a12fb56
LAPTOP-9V17EEST\Mark <yishsunsuncode@gmail.com>
2022-08-13 01:21:39 +0800

    feat(Utility): ReactiveCommand與Dispose

---
+++ IntervalzeroHomework/Utility/Extension/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reactive.Disposables;
+
+namespace Utility.Extension
+{
+    public static class ExtensionMethods
+    {
+        public static void DisposedBy(this IDisposable disposer, CompositeDisposable composite) 
+        {
+            composite.Add(disposer);
+        }
+    }
+}
---
+++ IntervalzeroHomework/Utility/Extension/ReactiveCommand.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Reactive.Subjects;
+using System.Windows.Input;
+
+namespace Utility.Extension
+{
+    public static class ReactiveCommand
+    {
+        public static ICommand Create(Action action, IObservable<bool> conditionSequence) 
+        {
+            bool localCondition = true;
+
+            var command = new RelayCommand(action, () => localCondition);
+            conditionSequence.Subscribe(cond =>
+            {
+                localCondition = cond;
+                command.RaiseCanExecuteChanged();
+            });
+
+            return command;
+        }
+    }
+}
